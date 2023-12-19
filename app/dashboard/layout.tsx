0772fe007536f7d8849515269fa74f4e252e0b33
Ethan <ethan@cmode.co.uk>
2023-12-19 09:49:24 +0000

    init

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="md:overlow-y-auto flex-grow p-6 md:p-12" />
+      <div className="md:overlow-y-auto flex-grow p-6 md:p-12">{children}</div>
     </div>
   );
 }
